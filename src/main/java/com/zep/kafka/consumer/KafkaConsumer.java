39928019fb39308bb8e9797b50fd489e825cb9dd
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-01 02:01:00 -0700

    consume student payload

--- src/main/java/com/zep/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/zep/kafka/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.zep.kafka.consumer;
 
+import com.zep.kafka.payload.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -7,8 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class KafkaConsumer {
-    @KafkaListener(topics ="zep",groupId = "myGroup")
+   // @KafkaListener(topics ="zep",groupId = "myGroup")
     public  void consumeMsg(String msg){
        log.info(String.format("Consuming the message from zep topic: %s",msg));
+    }
+    @KafkaListener(topics ="zep",groupId = "myGroup")
+    public  void consumeJsonMsg(Student student){
+        log.info(String.format("Consuming the message from zep topic: %s",student));
     }
 }
