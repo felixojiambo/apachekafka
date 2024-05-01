8d31d89ba2e879d775731cff3b27f55adc7607b9
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-01 01:01:29 -0700

    create zeptopic

---
+++ src/main/java/com/zep/kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.zep.kafka.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic zepTopic(){
+        return TopicBuilder
+                .name("zep")
+                .build();
+    }
+}
